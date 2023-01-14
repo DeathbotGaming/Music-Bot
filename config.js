ef1231491e6605ef21b43b1f9edb6656c69be3c8
DeathbotGaming <61156296+DeathbotGaming@users.noreply.github.com>
2023-01-14 14:00:44 -0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    app: {
+        token: 'MTA0OTg5NTU1MTA1NzAxODg4MA.Gw6K0O.QvNJg02BcejaMmnnzdtCrCJK_FnlKPIANSR4zI',
+        playing: 'by ShadowVR❤️',
+        global: true,
+        guild: '973767786046312508'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: '',
+            commands: []
+        },
+        maxVol: 100,
+        leaveOnEnd: true,
+        loopMessage: false,
+        spotifyBridge: true,
+        defaultvolume: 75,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
